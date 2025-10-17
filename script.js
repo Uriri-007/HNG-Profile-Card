06e94b81d185d6345513111bea70e79b2aae8e83
Uri Okhai <okhaiuri@gmail.com>
2025-10-17 10:32:16 +0100

    Hardcode  all user details into the HTML

--- script.js
+++ script.js
@@ -10,36 +10,3 @@
   };
 })();
 
-const profileData = {
-  name: "Uri Okhai",
-  image: "",
-  title: "Frontend Web Developer",
-  address: "Age-Mowo",
-  bio: "I am a modern frontend web developer with an undying paddion for more knowledge.",
-  socialLinks: [
-    {
-      type: "LinkedIn",
-      link: "",
-      iconClass: "fa-brands fa-linked-in",
-    },
-    {
-      type: "Github",
-      link: "",
-      iconClass: "fa-brands fa-github",
-    },
-    {
-      type: "X (twitter)",
-      link: "",
-      iconClass: "fa-brands fa-x-twitter",
-    }
-  ],
-  skills: [
-    "HTML5",
-    "CSS3",
-    "JavaScript",
-    "Version Control (Git)",
-    "ReactJS",
-    "Bootstrap",
-    "Tailwind CSS"
-  ]
-};
